2edbd1624ba7ca899e3a7fb57d6445fa30e48837
SE-Magellan <142025461+SE-Magellan@users.noreply.github.com>
2024-01-19 15:19:52 -0300

    initial commit
    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import flag
+
+st.radio('Test flag icon :smile:', (flag.flag('BR'), flag.flag('US'), flag.flag('GBENG') ))
+
+
+from streamlit_image_select import image_select
+
+
+langugae_option = {
+    'US': 'American English',
+    'GB': 'British English',
+    'BR': 'Português',
+}
+
+selected_language = st.radio('Language choice:', options=langugae_option)
+st.write(f'Selected language: {langugae_option[selected_language]}')
